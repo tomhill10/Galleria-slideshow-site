88342c2882a550cba893932e824cc2da88d409f1
tomhill10 <tom@codehill.co.uk>
2023-05-04 15:36:41 +0100

    Layout monstly implemented for slideshow

--- scripts/selectartist.js
+++ scripts/selectartist.js
@@ -3,13 +3,43 @@ $.ajax("../data/data.json").then((data) => {
   const selectedArtist = data[selectedArtistIndex];
   const artistContainer = $(".artist-container");
   const div = $("<div class='artist'>");
-  div.html(`
-    <div class="text-wrapper">
-      <h3 class="heading-3">${selectedArtist.name}</h3>
-      <p class="subhead-2">${selectedArtist.artist.name}</p>
-      <p>${selectedArtist.description}</p>
+  div.html(` 
+
+<div class="artist-wrapper">
+<div class="artist-left">
+    <div class="artist-left-name-artistName">
+		<h2 class="artist-left-name-artistName-h2">${selectedArtist.name}</h2>
+        <p class="artist-left-name-artistName-p">${selectedArtist.artist.name}</p>
     </div>
-    <img src="../${selectedArtist.images.gallery}" alt="artwork">
+    <div class="artist-left-image">
+        <img src=".${selectedArtist.images.hero.small}" alt="artist-image">
+        <button class="artist-left-image-button">
+            <img src="../assets/OpenImage.svg" alt="icon"> VIEW IMAGE </button>
+    </div>
+    <div class="artist-left-portrait">
+        <img src=".${selectedArtist.artist.image}" alt="thumbnail">
+    </div>
+</div>
+
+<div class="artist-right">
+    <h1 class="display artist-right-display">${selectedArtist.year}</h1>
+    <p class="body artist-right-body">${selectedArtist.description}</p>
+    <p class="subhead-1 artist-right-p">GO TO SOURCE</p>
+</div>
+</div>
+
+
+<footer class="artist-footer">
+    <div class="artist-footer-info">
+        <h3 class="heading-2 artist-footer-h3">${selectedArtist.name}</h3>
+        <p class="subhead-1 artist-footer-p">${selectedArtist.artist.name}</p>
+    </div>
+    <div class="artist-footer-arrows-wrapper">
+        <img src="../assets/Arrow-left.svg" alt="arrow">
+        <img src="../assets/Arrow-right.svg" alt="arrow">
+</footer>
+
+   
   `);
   artistContainer.append(div);
 });

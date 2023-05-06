46bdb4397220397cb6975c068af9ed3880c20ac1
tomhill10 <tom@codehill.co.uk>
2023-05-06 17:57:50 +0100

    changed from JQ to 'fetch()'

--- scripts/app.js
+++ scripts/app.js
@@ -1,19 +1,23 @@
-const artists = $(".artists");
+const artistsContainer = document.querySelector(".artists");
 
-$.ajax("./data/data.json").then((data) => {
-  data.forEach((artist, index) => {
-    const div = $("<div class='artist gallery-artist'>");
-    div.html(`
-      <div class="text-wrapper">
-        <h3 class="heading-3">${artist.name}</h3>
-        <p class="subhead-2">${artist.artist.name}</p>
-      </div>
-      <img src="${artist.images.gallery}" alt="artwork">
-    `);
-    div.on("click", function () {
-      localStorage.setItem("selectedArtistIndex", index);
-      window.location.href = "./pages/slideshow.html";
+fetch("./data/data.json")
+  .then((response) => response.json())
+  .then((json) => {
+    json.forEach((artist, index) => {
+      console.log("artist index:", index);
+      const div = document.createElement("div");
+      div.className = "artist gallery-artist";
+      div.innerHTML = `
+        <div class="text-wrapper">
+          <h3 class="heading-3">${artist.name}</h3>
+          <p class="subhead-2">${artist.artist.name}</p>
+        </div>
+        <img src="${artist.images.gallery}" alt="artwork">
+      `;
+      div.addEventListener("click", function () {
+        localStorage.setItem("selectedArtistIndex", index);
+        window.location.href = "./pages/slideshow.html";
+      });
+      artistsContainer.appendChild(div);
     });
-    artists.append(div);
   });
-});
--- scripts/selectartist.js
+++ scripts/selectartist.js
@@ -1,49 +1,48 @@
-$.ajax("../data/data.json").then((data) => {
-  const selectedArtistIndex = localStorage.getItem("selectedArtistIndex");
-  const selectedArtist = data[selectedArtistIndex];
-  const artistContainer = $(".artist-container");
-  const div = $("<div class='artist'>");
-  div.html(` 
+fetch("../data/data.json")
+  .then((response) => response.json())
+  .then((data) => {
+    const selectedArtistIndex = localStorage.getItem("selectedArtistIndex");
+    const selectedArtist = data[selectedArtistIndex];
+    const artistContainer = document.querySelector(".artist-container");
+    const div = document.createElement("div");
+    div.className = "artist";
+    div.innerHTML = `
+      <div class="artist-wrapper">
+        <div class="artist-left">
+          <div class="artist-left-name-artistName">
+            <h2 class="artist-left-name-artistName-h2">${selectedArtist.name}</h2>
+            <p class="artist-left-name-artistName-p">${selectedArtist.artist.name}</p>
+          </div>
+          <div class="artist-left-image">
+            <img src=".${selectedArtist.images.hero.small}" alt="artist-image">
+            <button class="artist-left-image-button">
+              <img src="../assets/OpenImage.svg" alt="icon"> VIEW IMAGE
+            </button>
+          </div>
+          <div class="artist-left-portrait">
+            <img src=".${selectedArtist.artist.image}" alt="thumbnail">
+          </div>
+        </div>
+        <div class="artist-right">
+          <h1 class="display artist-right-display">${selectedArtist.year}</h1>
+          <p class="body artist-right-body">${selectedArtist.description}</p>
+          <a href="${selectedArtist.source}" class="subhead-1 artist-right-a">GO TO SOURCE</a>
+        </div>
+      </div>
+      <footer class="artist-footer">
+        <div class="artist-footer-info">
+          <h3 class="heading-2 artist-footer-h3">${selectedArtist.name}</h3>
+          <p class="subhead-1 artist-footer-p">${selectedArtist.artist.name}</p>
+        </div>
+        <div class="artist-footer-arrows-wrapper">
+          <img src="../assets/Arrow-left.svg" alt="arrow">
+          <img src="../assets/Arrow-right.svg" alt="arrow">
+        </div>
+      </footer>
+    `;
+    artistContainer.appendChild(div);
+  });
 
-<div class="artist-wrapper">
-<div class="artist-left">
-    <div class="artist-left-name-artistName">
-		<h2 class="artist-left-name-artistName-h2">${selectedArtist.name}</h2>
-        <p class="artist-left-name-artistName-p">${selectedArtist.artist.name}</p>
-    </div>
-    <div class="artist-left-image">
-        <img src=".${selectedArtist.images.hero.small}" alt="artist-image">
-        <button class="artist-left-image-button">
-            <img src="../assets/OpenImage.svg" alt="icon"> VIEW IMAGE </button>
-    </div>
-    <div class="artist-left-portrait">
-        <img src=".${selectedArtist.artist.image}" alt="thumbnail">
-    </div>
-</div>
-
-<div class="artist-right">
-    <h1 class="display artist-right-display">${selectedArtist.year}</h1>
-    <p class="body artist-right-body">${selectedArtist.description}</p>
-    <a href="${selectedArtist.source}" class="subhead-1 artist-right-a">GO TO SOURCE</a>
-</div>
-</div>
-
-
-<footer class="artist-footer">
-    <div class="artist-footer-info">
-        <h3 class="heading-2 artist-footer-h3">${selectedArtist.name}</h3>
-        <p class="subhead-1 artist-footer-p">${selectedArtist.artist.name}</p>
-    </div>
-    <div class="artist-footer-arrows-wrapper">
-        <img src="../assets/Arrow-left.svg" alt="arrow">
-        <img src="../assets/Arrow-right.svg" alt="arrow">
-</footer>
-
-   
-  `);
-  artistContainer.append(div);
-});
-
-window.onbeforeunload = function () {
-  localStorage.removeItem("selectedArtistIndex");
-};
+// window.onbeforeunload = function () {
+//   localStorage.removeItem("selectedArtistIndex");
+// };
